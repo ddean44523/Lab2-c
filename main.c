b484f03356d01054790e87f5b7e70196813fcaed
DidierOne <dpd5518@psu.edu>
2020-09-08 15:04:40 +0000

    {}

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
 // Author: Daniel Dean dpd@psu.edu
-// Collaborator: Yash Patni 
-// Collaborator: Cameron Ezeiraku
-// Collaborator: Stephanie Huang
+// Collaborator: Yash Patni  yjp5077@psu.edu
+// Collaborator: Cameron Ezeiraku cke5142@psu.edu
+// Collaborator: Stephanie Huang ssh5302@psu.edu
 // Section: 2
 // Breakout: 7
 #include <stdio.h>
@@ -12,23 +12,32 @@ int main(void) {
   char * gradeInput;
   gradeInput = readline("Enter your CMPSC 131 grade: ");
   double grade = atof(gradeInput);
-  if(grade >= 93.0)
-   printf("Your letter grade for CMPSC 131 is A.\n");
-  else if(grade >= 90.0)
-   printf("Your letter grade for CMPSC 131 is A-.\n");
-  else if(grade >= 87.0)
-   printf("Your letter grade for CMPSC 131 is B+.\n");
-  else if(grade >= 83.0)
+  if(grade >= 93.0){
+    printf("Your letter grade for CMPSC 131 is A.\n");
+  }
+  else if(grade >= 90.0){
+    printf("Your letter grade for CMPSC 131 is A-.\n");
+  }
+  else if(grade >= 87.0){
+    printf("Your letter grade for CMPSC 131 is B+.\n");
+  }
+  else if(grade >= 83.0){
     printf("Your letter grade for CMPSC 131 is B.\n");
-  else if(grade >= 80.0)
-   printf("Your letter grade for CMPSC 131 is B-.\n");
-  else if(grade >= 77.0)
-   printf("Your letter grade for CMPSC 131 is C+.\n");
-  else if(grade >= 70.0)
-   printf("Your letter grade for CMPSC 131 is C.\n");
-  else if(grade >= 60)
-   printf("Your letter grade for CMPSC 131 is D.\n");
-  else if(grade < 60)
-   printf("Your letter grade for CMPSC 131 is F.\n");
+  }
+  else if(grade >= 80.0){
+    printf("Your letter grade for CMPSC 131 is B-.\n");
+  }
+  else if(grade >= 77.0){
+    printf("Your letter grade for CMPSC 131 is C+.\n");
+  }
+  else if(grade >= 70.0){
+    printf("Your letter grade for CMPSC 131 is C.\n");
+  }
+  else if(grade >= 60){
+    printf("Your letter grade for CMPSC 131 is D.\n");
+  }
+  else if(grade < 60){
+    printf("Your letter grade for CMPSC 131 is F.\n");
+  }
   return 0;
 } 
